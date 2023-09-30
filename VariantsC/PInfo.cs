31ad6643a7e4bdef6963bc0d7473dac1d24056f1
Neoshrimp <shadow638@protonmail.com>
2023-10-01 01:45:37 +0300

    innit commit

---
+++ VariantsC/PInfo.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace VariantsC
+{
+    public static class PInfo
+    {
+        // each loaded plugin needs to have a unique GUID. usually author+generalCategory+Name is good enough
+        public const string GUID = "neo.lbol.gameplay.VariantsC";
+        public const string Name = "Variants C";
+        public const string version = "1.0.0";
+        public static readonly Harmony harmony = new Harmony(GUID);
+
+    }
+}
