58baec86d650e335c2a86eaec1a077508fc46e05
Neoshrimp <shadow638@protonmail.com>
2024-03-10 17:23:18 +0200

    Switch to batchLoc

--- FullElite/Jadeboxes.cs
+++ FullElite/Jadeboxes.cs
@@ -37,11 +37,7 @@ namespace FullElite
 
         public const string JdBoxGroup = "FullElite";
 
-        public override LocalizationOption LoadLocalization() 
-        {
-            var gl = new GlobalLocalization(BepinexPlugin.embeddedSource);
-            return gl;
-        }
+        public override LocalizationOption LoadLocalization() => jadeboxBatchLoc.AddEntity(this);
 
         public override JadeBoxConfig MakeConfig()
         {
@@ -77,14 +73,6 @@ namespace FullElite
     {
         public override IdContainer GetId() => nameof(FullEliteBox);
 
-        public override LocalizationOption LoadLocalization()
-        {
-            var gl = new GlobalLocalization(BepinexPlugin.embeddedSource);
-            gl.LocalizationFiles.AddLocaleFile(LBoL.Core.Locale.En, "JadeboxesEn");
-            gl.LocalizationFiles.AddLocaleFile(LBoL.Core.Locale.Ko, "JadeboxesKo");
-
-            return gl;
-        }
 
         [EntityLogic(typeof(FullEliteJadeboxDef))]
         public sealed class FullEliteBox : BaseFullElite
@@ -167,7 +155,7 @@ namespace FullElite
     {
         public override IdContainer GetId() => nameof(StartingDraftBox);
 
-        public override LocalizationOption LoadLocalization() => new GlobalLocalization(embeddedSource);
+        public override LocalizationOption LoadLocalization() => jadeboxBatchLoc.AddEntity(this);
         public override JadeBoxConfig MakeConfig()
         {
             var con = DefaultConfig();
@@ -211,7 +199,7 @@ namespace FullElite
     {
         public override IdContainer GetId() => nameof(PowerBonusBox);
 
-        public override LocalizationOption LoadLocalization() => new GlobalLocalization(embeddedSource);
+        public override LocalizationOption LoadLocalization() => jadeboxBatchLoc.AddEntity(this);
         public override JadeBoxConfig MakeConfig()
         {
             var con = DefaultConfig();
@@ -234,7 +222,7 @@ namespace FullElite
     {
         public override IdContainer GetId() => nameof(BlockToysBonusBox);
 
-        public override LocalizationOption LoadLocalization() => new GlobalLocalization(embeddedSource);
+        public override LocalizationOption LoadLocalization() => jadeboxBatchLoc.AddEntity(this);
         public override JadeBoxConfig MakeConfig()
         {
             var con = DefaultConfig();
