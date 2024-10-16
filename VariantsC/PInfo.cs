18cec4c897a8255809e52bb05ec1753e2ed96cbe
Neoshrimp <shadow638@protonmail.com>
2024-10-16 12:37:10 +0300

    Change VariantC DirResources folder structure

--- VariantsC/PInfo.cs
+++ VariantsC/PInfo.cs
@@ -7,7 +7,7 @@ namespace VariantsC
         // each loaded plugin needs to have a unique GUID. usually author+generalCategory+Name is good enough
         public const string GUID = "neo.lbol.gameplay.VariantsC";
         public const string Name = "Variants C";
-        public const string version = "0.5.4200";
+        public const string version = "0.5.4220";
         public static readonly Harmony harmony = new Harmony(GUID);
 
     }
